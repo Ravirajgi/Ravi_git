c7952fde067af575cbb21898bb77fef76a99f480
Dell <Dell@DESKTOP-MTSLE7D>
2022-07-27 16:22:51 +0530

    [raviraj]:added new project

---
+++ src/sample1/demo1.java
@@ -0,0 +1,17 @@
+package sample1;
+
+public class demo1 
+{
+
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.println("hi");
+		System.out.println("my");
+		System.out.println("Name");
+		System.out.println("is");
+		System.out.println("ravi");
+		
+	}
+}
